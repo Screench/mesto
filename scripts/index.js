2fc78f0e823ab64a531c3bdddb3662f695c138c4
Screench <screench@yandex.ru>
2023-02-19 22:13:45 +0300

    Fixes after first review

--- scripts/index.js
+++ scripts/index.js
@@ -10,14 +10,21 @@ closeButton.addEventListener('click', popupToggle);
 let submitButton = document.querySelector('.popup__submit-btn');
 submitButton.addEventListener('submit', handleFormSubmit);
 
-//Открытие и закрытие Попапа
-function popupToggle() {
-  let popup = document.querySelector('.popup');
-  popup.classList.toggle('popup__opened');
-}
+let popup = document.querySelector('.popup');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__input_field_name');
+let jobInput = document.querySelector('.popup__input_field_occupation');
+// Выберите элементы, куда должны быть вставлены значения полей
+let existingUserName = document.querySelector('.profile__title');
+let existingOccupation = document.querySelector('.profile__occupation');
 
 // Находим форму в DOM
 let formElement = document.querySelector('form');
+
+//Открытие и закрытие Попапа
+function popupToggle() {
+  popup.classList.toggle('popup__opened');
+}
 
 //По кнопке Редактировать открываем попап и загружаем в инпуты текст из HTML. 
 //Реализовано с помощью поочередного вызова соответствующих функций
@@ -27,12 +34,6 @@ function editButtonFunctions() {
 }
 
 function updateInputsFromForm() {
-  // Находим поля формы в DOM
-  let nameInput = document.querySelector('.popup__input_name');
-  let jobInput = document.querySelector('.popup__input_occupation');
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let existingUserName = document.querySelector('.profile__title');
-  let existingOccupation = document.querySelector('.profile__occupation');
   // Получите значение полей jobInput и nameInput из свойства value
   jobInput.value = existingOccupation.textContent;
   nameInput.value = existingUserName.textContent;
@@ -41,12 +42,7 @@ function updateInputsFromForm() {
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. 
-  // Находим поля формы в DOM
-  let nameInput = document.querySelector('.popup__input_name');
-  let jobInput = document.querySelector('.popup__input_occupation');
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let existingUserName = document.querySelector('.profile__title');
-  let existingOccupation = document.querySelector('.profile__occupation');
+  
   // Вставьте новые значения с помощью textContent
   existingUserName.textContent = nameInput.value;
   existingOccupation.textContent = jobInput.value;
