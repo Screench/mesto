d1b881f289ba34e28ac526373aa055e9ee3e5971
Alexander Abilov <screench@yandex.ru>
2023-03-01 14:19:56 +0300

    completed first 5 tasks in YP assignment

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,30 @@
-//Кнопка Редактировать
-let editButton = document.querySelector('.profile__edit-btn');
-//Кнопка Закрыть
-let closeButton = document.querySelector('.popup__close-btn');
-//Находим попап в DOM
-let popup = document.querySelector('.popup');
-// Находим поля формы в DOM
+///////////////////////////Проектная работа №4 (Попап редактирование профиля)
+let editButton = document.querySelector('.profile__edit-btn');      //Кнопка Редактировать
+let closeButton = document.querySelector('.popup__close-btn');                          //Кнопка Закрыть
+let popupProfile = document.querySelector('.popup_type_profile');                       //Находим попап профиля в DOM
+
+// Находим поля формы профиля в DOM
 let nameInput = document.querySelector('.popup__input_field_name');
 let jobInput = document.querySelector('.popup__input_field_occupation');
+let placeInput = document.querySelector('.popup__input_field_name');
+
 // Находим элементы, куда должны быть вставлены значения полей
 let existingUserName = document.querySelector('.profile__title');
 let existingOccupation = document.querySelector('.profile__occupation');
-// Находим форму в DOM
-let formElement = document.querySelector('form');
 
-//Слушатель к кнопке редактировать
-editButton.addEventListener('click', editButtonFunctions);
-//Слушатель к кнопке закрыть
-closeButton.addEventListener('click', popupToggle);
+let formElement = document.querySelector('.profile-form');    // Находим форму профиля в DOM
+editButton.addEventListener('click', editButtonFunctions);    //Слушатель к кнопке редактировать
+closeButton.addEventListener('click', popupProfileToggle);    //Слушатель к кнопке закрыть на профиле
 
-//Открытие и закрытие Попапа
-function popupToggle() {
-  popup.classList.toggle('popup_opened');
+//Открытие и закрытие Попапа с профилем
+function popupProfileToggle() {
+  popupProfile.classList.toggle('popup_opened');
 }
 
 //По кнопке Редактировать открываем попап и загружаем в инпуты текст из HTML. 
 //Реализовано с помощью поочередного вызова соответствующих функций
 function editButtonFunctions() {
-  popupToggle();
+  popupProfileToggle();
   updateInputsFromForm();
 }
 
@@ -36,24 +34,24 @@ function updateInputsFromForm() {
   nameInput.value = existingUserName.textContent;
 }
 
-// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+// Обработчик «отправки» формы профиля, хотя пока она никуда отправляться не будет
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. 
-
-  // Вставьте новые значения с помощью textContent
-  existingUserName.textContent = nameInput.value;
-  existingOccupation.textContent = jobInput.value;
-  //Закрытие окна
-  popupToggle();
+  existingUserName.textContent = nameInput.value; // Вставьте новые значения с помощью textContent
+  existingOccupation.textContent = jobInput.value;// Вставьте новые значения с помощью textContent
+  popupProfileToggle(); //Закрытие окна
 }
 
-// Прикрепляем обработчик к форме:
+// Прикрепляем обработчик к форме профиля:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
 
 
 
-const cards = [
+
+///////////////////////////ПРОЕКТНАЯ РАБОТА №5. По заданиям:
+///////////////////////////1. Шесть карточек из "коробки"
+const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -78,15 +76,88 @@ const cards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-]; 
+];
+
+initialCards.reverse();
 
 const elements = document.querySelector('.elements');
 
-cards.forEach(function(card) {
+initialCards.forEach(createCard);
+
+function createCard(card) {
   const newCard = document.querySelector('.cardTemplate').content.cloneNode(true);
   const cardHeading = newCard.querySelector('.element__title');
   const cardImage = newCard.querySelector('.element__image')
+  const deleteButton = newCard.querySelector('.element__trash-btn');
+  const likeButton = newCard.querySelector('.element__heart-btn');
+  likeButton.addEventListener('click', handleLikeButtonClick);
+  deleteButton.addEventListener('click', handleDeleteButtonClick);
   cardHeading.textContent = card.name;
   cardImage.setAttribute('src', card.link);
-  elements.append(newCard);
-});
+  cardImage.setAttribute('alt', `Изображение ${card.name}`);
+  elements.prepend(newCard);
+};
+
+
+
+///////////////////////////2. Форма добавления карточки
+
+let addButton = document.querySelector('.profile__add-btn');        //Кнопка Добавить
+let popupPlace = document.querySelector('.popup_type_place');       //Находим попап новой карточки в DOM
+let closeButtonOnAddPlace = document.querySelector('.popup_type_place .popup__close-btn'); //Находим кнопку закрытия попапа новой карточки
+let formPlace = document.querySelector('.place-form');              // Находим форму новой карточки в DOM
+addButton.addEventListener('click', addButtonFunctions);            //Слушатель к кнопке добавить карточку
+closeButtonOnAddPlace.addEventListener('click', popupPlaceToggle);  //Слушатель к кнопке закрыть на добавлении карточки
+
+//По кнопке "Добавить" открываем попап 
+function addButtonFunctions() {
+  formPlace.reset();
+  popupPlaceToggle();
+}
+
+function popupPlaceToggle() {
+  popupPlace.classList.toggle('popup_opened');
+}
+
+
+///////////////////////////3. Добавление карточки
+
+formPlace.addEventListener('submit', handleFormSubmitPlace);    // Прикрепляем обработчик к форме новой карточки:
+
+// Обработчик «отправки» формы новой карточки
+function handleFormSubmitPlace(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. 
+
+  const form = evt.target;
+  const name = form.querySelector('.popup__input_field_place').value;
+  const link = form.querySelector('.popup__input_field_link').value;
+  const card = {
+    name: name,
+    link: link,
+  }
+  createCard(card);
+
+  popupPlaceToggle();   //Закрытие окна
+
+}
+
+
+///////////////////////////4. Лайк карточки
+function handleLikeButtonClick(event) {
+  const likeBtn = event.target;
+  event.target.classList.toggle('element__heart-btn_active');
+};
+
+
+///////////////////////////5. Удаление карточки
+function handleDeleteButtonClick(event) {
+  const trashBtn = event.target;
+  const element = trashBtn.closest('.element');
+  element.remove();
+}
+
+
+///////////////////////////6. Открытие попапа с картинкой
+
+
+///////////////////////////7. Плавное открытие и закрытие попапов
